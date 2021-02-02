44928d64228cff69787ee5cd1563e35d78ca7698
roshni-patel <rpatel01@wesleyan.edu>
2021-02-01 21:04:56 -0600

    Refactored process news method, and updated get raw news api call to grab 100 articles

--- good_news/api/news.py
+++ good_news/api/news.py
@@ -5,22 +5,34 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 from datetime import datetime, timedelta
+import urllib 
 
 
 def get_raw_news():
     NEWS_API_KEY = os.getenv('X_API_KEY')
     headers = {'X-Api-Key': NEWS_API_KEY}
     d = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
-    response = requests.get(f'https://newsapi.org/v2/everything?sources=Buzzfeed,Google News, New York Times, The Huffington Post, CNN, NBC News, The Washington Post, ABC News, BBC News, Next Big Future, New Scientist, Medical News Today, TechCrunch, Ars Technica, Wired, The Next Web, Bloomberg, The Wall Street Journal, Business Insider&language=en&from={d}', headers=headers)
+    params = { 
+        'sources': ','.join(["Buzzfeed","Google News", "New York Times", "The Huffington Post", "CNN", "NBC News", "The Washington Post", "ABC News", "BBC News", "Next Big Future", "New Scientist", "Medical News Today", "TechCrunch", "Ars Technica", "Wired", "The Next Web", "Bloomberg", "The Wall Street Journal", "Business Insider"]),
+        'language': 'en', 
+        'from': d, 
+        'pageSize': 100, 
+        'sortBy': 'popularity'
+    }
+    # print(f'https://newsapi.org/v2/everything?{urllib.parse.urlencode(params)}')
+    response = requests.get(f'https://newsapi.org/v2/everything?{urllib.parse.urlencode(params)}', headers=headers)
     return response.json() 
 
 
 def get_good_news():
     raw_news = get_raw_news()
+    # print(f'raw news', len(raw_news['articles']))
     good_news = process_news(raw_news)
+    # print(f'good news', len(good_news))
+
     return good_news
     
-    
+
 def process_news(news):
     AZURE_KEY = os.getenv('OCP_APIM_SUBSCRIPTION_KEY')
     headers = {'Ocp-Apim-Subscription-Key': AZURE_KEY, 'Content-Type': 'application/json', 'Accept': 'application/json'}
@@ -35,7 +47,10 @@ def process_news(news):
         article = news['articles'][i]
         request_body["documents"].append({"language": "en", "id": str(i), "text": article["title"]})
         batch_count += 1 
-        if batch_count == 10:
+        # in a batch of 44, triggers after 10, 20, 30, 40, 44
+        # in a batch of 43, triggers after 10, 20, 30, 40, 43
+
+        if batch_count == 10 or i + 1 == len(news['articles']):
             response = requests.post('https://good-news-test.cognitiveservices.azure.com/text/analytics/v3.0/sentiment', json = request_body, headers=headers)
             json_data = response.json()
             for result in json_data["documents"]:
@@ -43,14 +58,15 @@ def process_news(news):
                 news["articles"][article_pos]["sentiment"] = result 
             batch_count = 0 
             request_body = {"documents": []}
-    if batch_count > 0: 
-        # we had some partial
-        response = requests.post('https://good-news-test.cognitiveservices.azure.com/text/analytics/v3.0/sentiment', json = request_body, headers=headers)
-        json_data = response.json()
-        # print(json_data)
-        for result in json_data["documents"]:
-            article_pos = int(result["id"])
-            news["articles"][article_pos]["sentiment"] = result 
+    # if batch_count > 0: 
+    #     # we had some partial
+    #     response = requests.post('https://good-news-test.cognitiveservices.azure.com/text/analytics/v3.0/sentiment', json = request_body, headers=headers)
+    #     json_data = response.json()
+    #     # print(json_data)
+    #     for result in json_data["documents"]:
+    #         article_pos = int(result["id"])
+    #         news["articles"][article_pos]["sentiment"] = result 
+    # print(f'image news', len(news['articles']))
     return filter_news(news) 
     # return news
 
@@ -63,6 +79,6 @@ def process_news(news):
 def filter_news(news):
     filtered = []
     for article in news['articles']:
-        if article["sentiment"]["sentiment"] == "neutral" or article["sentiment"]["sentiment"] == "positive":
+        if article["sentiment"]["sentiment"] == "positive":
             filtered.append(article)
     return filtered 
